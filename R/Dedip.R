2bc6eebb954d06373461698ac5e9ece16370f351
Louis Aslett <louis.aslett@stats.ox.ac.uk>
2017-04-13 18:03:49 +0100

    Add additive model de-diploiding.

--- R/Dedip.R
+++ R/Dedip.R
@@ -4,6 +4,7 @@ Dedip <- function(fwd, bck, method="min") {
                 min2nd = new("dspMatrix", Dim=as.integer(c(nrow(fwd)/2,nrow(fwd)/2)), x=Dedip_2nd_min(fwd, bck), uplo="L"),
                 dom    = new("dspMatrix", Dim=as.integer(c(nrow(fwd)/2,nrow(fwd)/2)), x=Dedip_dom(fwd, bck), uplo="L"),
                 max    = new("dspMatrix", Dim=as.integer(c(nrow(fwd)/2,nrow(fwd)/2)), x=Dedip_max(fwd, bck), uplo="L"),
+                add    = new("dspMatrix", Dim=as.integer(c(nrow(fwd)/2,nrow(fwd)/2)), x=Dedip_add(fwd, bck), uplo="L"),
                 mean   = new("dspMatrix", Dim=as.integer(c(nrow(fwd)/2,nrow(fwd)/2)), x=Dedip_mean(fwd, bck), uplo="L"),
                 all    = {
                   res <- Dedip_all(fwd, bck)
@@ -11,7 +12,8 @@ Dedip <- function(fwd, bck, method="min") {
                        min2nd = new("dspMatrix", Dim=as.integer(c(nrow(fwd)/2,nrow(fwd)/2)), x=res[[2]], uplo="L"),
                        dom    = new("dspMatrix", Dim=as.integer(c(nrow(fwd)/2,nrow(fwd)/2)), x=res[[3]], uplo="L"),
                        max    = new("dspMatrix", Dim=as.integer(c(nrow(fwd)/2,nrow(fwd)/2)), x=res[[4]], uplo="L"),
-                       mean   = new("dspMatrix", Dim=as.integer(c(nrow(fwd)/2,nrow(fwd)/2)), x=res[[5]], uplo="L"))
+                       add    = new("dspMatrix", Dim=as.integer(c(nrow(fwd)/2,nrow(fwd)/2)), x=res[[5]], uplo="L"))
+                       mean   = new("dspMatrix", Dim=as.integer(c(nrow(fwd)/2,nrow(fwd)/2)), x=res[[6]], uplo="L"))
                 },
                 stop("Unknown method")))
 }
