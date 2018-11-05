91f52e7b20e8c63e68c4fb7adae71209c31e68a2
Louis Aslett <louis.aslett@durham.ac.uk>
2018-11-05 20:07:33 +0000

    Correctly import digest dependency for new parameter environments.

--- R/kalis.R
+++ R/kalis.R
@@ -2,6 +2,7 @@
 #' @importFrom glue glue glue_collapse
 #' @import rhdf5
 #' @importFrom rlang duplicate
+#' @importFrom digest digest
 #' @importFrom stringr str_split_fixed
 #'
 #' @useDynLib kalis
