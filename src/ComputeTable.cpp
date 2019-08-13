3c645506be1a494809a11ed7c839c2b14321b21e
Louis Aslett <louis.aslett@durham.ac.uk>
2019-08-13 17:59:57 +0100

    Remove redundant C++

--- src/ComputeTable.cpp
+++
@@ -1,7 +0,0 @@
-#include <Rcpp.h>
-using namespace Rcpp;
-
-// [[Rcpp::export]]
-NumericMatrix Exact_ComputeTable_naive_cpp(int l, NumericMatrix Pi, NumericVector mu, NumericVector rho) {
-  return(Pi);
-}
