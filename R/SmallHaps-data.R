c9df108f5266587614210a0fbc82285af3111601
Louis Aslett <louis.aslett@durham.ac.uk>
2019-09-28 19:51:06 +0100

    Documentation tidying for the simulated data.

--- R/SmallHaps-data.R
+++ R/SmallHaps-data.R
@@ -1,18 +1,23 @@
 #' SmallHaps Example Phased Haplotype Dataset for kalis
 #'
-#' Simulated dataset with N=100 haplotypes at L=500 loci generated using msprime (see reference).
+#' Simulated dataset with N=100 haplotypes at L=500 loci generated using msprime
+#' (Kelleher et al., 2016).
 #'
 #' @docType data
 #'
 #' @usage data(SmallHaps)
 #'
-#' @format An object of class \code{"matrix"} where each column is a haplotype.
+#' @format
+#'   An object of class \code{matrix} where each column is a
+#'   simulated haplotype.
+#'   This is suitable for passing directly to \code{\link{CacheHaplotypes}}.
 #'
 #' @keywords datasets
 #'
-#' @references J Kelleher, et al. (2016) PLoS Comput Biol 12(5): e1004842.
-#' (\href{https://doi.org/10.1371/journal.pcbi.1004842}{Article})
-#'
+#' @references
+#'   Kelleher, J., Etheridge, A. M., & McVean, G. (2016). Efficient coalescent
+#'   simulation and genealogical analysis for large sample sizes.
+#'   *PLoS computational biology*, **12**(5).
 #'
 #' @examples
 #' \dontrun{
@@ -21,10 +26,12 @@
 #' hist(rowMeans(SmallHaps),breaks=20)
 #'
 #' # Store in kalis hdf5 format
-#' WriteIndividualHaplotypeH5("SmallHaps.h5",SmallHaps)
+#' WriteIndividualHaplotypeH5("SmallHaps.h5", SmallHaps)
 #'
-#' # Import into kalis cache
-#' CacheAllHaplotypesH5("SmallHaps.h5")
+#' # Import into kalis cache directly ...
+#' CacheHaplotypes(SmallHaps)
+#' # ... or via the HDF5 file written out above
+#' CacheHaplotypes("SmallHaps.h5")
 #' }
 #'
 "SmallHaps"
