973558ce238878e75c442fe18e29b950700f925e
Louis Aslett <louis.aslett@durham.ac.uk>
2019-09-28 13:40:26 +0100

    Eliminate cruft from when we used to load from .seq files.

--- R/GenWorkingDir.R
+++
@@ -1,9 +0,0 @@
-SetGenWorkingDir <- function(working.dir) {
-  if(is.na(file.info(working.dir)$isdir) || !file.info(working.dir)$isdir) {
-    stop(working.dir, " is not a directory.")
-  }
-  assign("working.dir", working.dir, envir = pkgVars)
-}
-GetGenWorkingDir <- function() {
-  get("working.dir", envir = pkgVars)
-}
